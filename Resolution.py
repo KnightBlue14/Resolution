560107722b4325c02f2134a20082effc605abfa3
Dom B <87432624+KnightBlue14@users.noreply.github.com>
2023-10-11 15:46:04 +0100

    Update Resolution.py
    Now supports manual selection for more resolutions. Edit code to allow more options

--- Resolution.py
+++ Resolution.py
@@ -1,19 +1,29 @@
-import tkinter as tk
 import pywintypes
 import win32con
 import win32api
+import time
 
-root = tk.Tk()
-height = root.winfo_screenheight()
 
 devmode = pywintypes.DEVMODEType()
+valid = 0
+while valid == 0:
+    heightinp = input('Set resolution: -- ')
+    if heightinp in ['720','1080','1440']:
+        valid += 1
+    else:
+        print('Invalid resolution. Please try again')
+        time.sleep(2)
 
-if height == 1080:
-    devmode.PelsWidth = 2560
-    devmode.PelsHeight =1440
-if height == 1440:
+
+if heightinp == '720':
+    devmode.PelsWidth = 1280
+    devmode.PelsHeight =720
+if heightinp == '1080':
     devmode.PelsWidth = 1920
-    devmode.PelsHeight = 1080
+    devmode.PelsHeight =1080
+if heightinp == '1440':
+    devmode.PelsWidth = 2560
+    devmode.PelsHeight = 1440
 
 devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
 
